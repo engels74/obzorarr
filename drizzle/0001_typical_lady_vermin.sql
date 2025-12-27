e3196f27d7184cf9fbca38f2ebfea12f021f7e2f
engels74 <141435164+engels74@users.noreply.github.com>
2025-12-27 12:09:16 +0100

    docs: add docs directory to gitignore
    refactor: delete old drizzle migration files

--- drizzle/0001_typical_lady_vermin.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE `plex_accounts` (
-	`account_id` integer PRIMARY KEY NOT NULL,
-	`plex_id` integer NOT NULL,
-	`username` text NOT NULL,
-	`thumb` text,
-	`is_owner` integer DEFAULT false,
-	`updated_at` integer
-);
